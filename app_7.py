e4f872da4612ff085fa62f4fe590657a4eb32c69
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-08-04 13:40:12 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -197,6 +197,7 @@ def main():
         if graph_type == 'Barras':
             sns.barplot(x = bank_raw_target_perc.index, 
                         y = "y",
+			hue = "status",
                         data = bank_raw_target_perc, 
                         ax = ax[0])
             ax[0].bar_label(ax[0].containers[0])
